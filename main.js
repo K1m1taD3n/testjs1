d9e1dd3aad7857cfa559c627cf2817a8435aace5
nikden34562 <nikolasmongrail@gmail.com>
2023-02-13 00:12:30 +0100

    added changes in color for footer and elements in CSS file and main file

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 console.log("test");
+
+
 const header = document.getElementById("first_header");
 header.innerText = "Main 1111";
-header.style.color = 'violet'
+header.style.color = 'violet';
 console.log('First header: ', header);
+
+const footer = document.getElementById("first paragraph");
+footer.style.color = 'white';
+console.log('First paragraph: ', footer)
+
+const elements = document.getElementById("elements");
+elements.style.color = 'green';
+console.log('Elements: ', elements)
