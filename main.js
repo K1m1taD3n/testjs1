14e8ed1a16c58f97a6027d3dbc559f1673b7822b
nikden34562 <nikolasmongrail@gmail.com>
2023-02-15 17:30:41 +0100

    Renamed to first_paragraph

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 console.log("test");
 
 
-const header = document.getElementById("first_header");
-header.innerText = "Main 1111";
-header.style.color = 'violet';
-console.log('First header: ', header);
+const first_paragraph = document.getElementById("first_paragraph");
+first_paragraph.innerText = "Main 1111";
+first_paragraph.style.color = 'violet';
+console.log('First paragraph: ', first_paragraph );
 
 const footer = document.getElementById("first paragraph");
 footer.style.color = 'white';
@@ -12,4 +12,5 @@ console.log('First paragraph: ', footer)
 
 const elements = document.getElementById("elements");
 elements.style.color = 'violet';
-console.log('Elements: ', elements)
+console.log('Elements: ', elements);
+
