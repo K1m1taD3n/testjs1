0d37bc871099fff2e7e65832cd188241ca7300fc
nikden34562 <nikolasmongrail@gmail.com>
2023-02-15 20:29:41 +0100

    added numeration

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 console.log("test");
 
-
 const first_paragraph = document.getElementById("first_paragraph");
 first_paragraph.innerText = "Main";
-first_paragraph.style.color = 'violet';
+first_paragraph.style.color = 'green';
 console.log('First paragraph: ', first_paragraph );
 
 const footer = document.getElementById("last paragraph");
-footer.style.color = 'yellow';
 console.log('First paragraph: ', footer)
 
 const elements = document.getElementById("elements");
-elements.style.color = 'white';
+elements.style.color = 'red';
 console.log('Elements: ', elements);
 
-const paragraph = document.getElementsByTagName("ul", [0]);
-console.log("Numeration: ", paragraph)
+const listItem = document.querySelector("ul");
+console.log('list ', listItem);
+
+const newItem = document.createElement('ol');
+newItem.innerHTML =
+    `<ol><li>elements 123</li>
+<li>elements 123</li>
+<li>elements 123</li>
+<li>elements 123</li>
+<li>elements 123</li>
+<li>elements 123</li>
+</ol>
+`;
+listItem.parentNode.replaceChild(newItem, listItem);
+
+
+
+
