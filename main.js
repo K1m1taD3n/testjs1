fab89f652917814e4cd62b8b5917c260641f32b9
nikden34562 <nikolasmongrail@gmail.com>
2023-02-15 18:33:56 +0100

    a little bit of polishing

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ console.log("test");
 
 
 const first_paragraph = document.getElementById("first_paragraph");
-first_paragraph.innerText = "Main 1111";
+first_paragraph.innerText = "Main";
 first_paragraph.style.color = 'violet';
 console.log('First paragraph: ', first_paragraph );
 
-const footer = document.getElementById("first paragraph");
-footer.style.color = 'white';
+const footer = document.getElementById("last paragraph");
+footer.style.color = 'yellow';
 console.log('First paragraph: ', footer)
 
 const elements = document.getElementById("elements");
-elements.style.color = 'violet';
+elements.style.color = 'white';
 console.log('Elements: ', elements);
 
+const paragraph = document.getElementsByTagName("ul", [0]);
+console.log("Numeration: ", paragraph)
